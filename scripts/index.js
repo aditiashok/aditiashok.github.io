8a0a534f057156b05f62e54fc7d819fb83a98c0b
Aditi Ashok <aditiashok@Aditis-MacBook-Pro.local>
2015-08-21 14:51:50 -0400

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 				$('#slideshow').cycle({
 				fx: 'fade',
 				pager: '#smallnav',
+				prev:   '#backButton',
+    	next:   '#forwardButton', 
 				pause:   0.5,
 				speed: 1800,
 				timeout:  3500
